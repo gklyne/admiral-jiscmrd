0ef8bc5616db0caccb3749fcded3dd9de8ab3235
bhavana <bhavana@localhost>
2010-12-14 15:21:05 +0000

    Moved Databank view links from the dataset submit summary page to the ADMIRAL dataset view page.

---
+++ src/SubmitDatasetHandler/cgi-bin/SubmitDatasetSummaryHandler.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+#
+# Coypyright (C) 2010, University of Oxford
+#
+# Licensed under the MIT License.  You may obtain a copy of the License at:
+#
+#     http://www.opensource.org/licenses/mit-license.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# $Id: $
+
+"""
+Main CGI program that handles response summary page after dataset submission to RDF Databank.
+"""
+__author__ = "Bhavana Ananda"
+__version__ = "0.1"
+
+import cgi, sys, os, logging
+
+sys.path.append("..")
+sys.path.append("../..")
+
+import SubmitDatasetUtils
+
+Logger                   =  logging.getLogger("SubmitDatasetSummaryHandler")
+
+def datasetSummaryForm(formdata, outputstr):
+    """
+    Print (to stdout) a new HTML page reflecting the outcome of the request.
+    
+    formdata    is a dictionary containing parameters from the dataset submission form / handler
+    """
+    siloName             =  "admiral-test"
+    save_stdout          =  sys.stdout
+    dirName              =  SubmitDatasetUtils.getFormParam("dir"     , formdata)
+    datasetName          =  SubmitDatasetUtils.getFormParam("id"      , formdata)
+    datasetUnzippedName  =  SubmitDatasetUtils.getFormParam("unzipid" , formdata)
+    status               =  SubmitDatasetUtils.getFormParam("status"  , formdata)  
+   
+    # print repr(formdata)
+
+    if outputstr:
+        sys.stdout = outputstr
+
+        # Generate response headers
+        print "Content-type: text/html"
+        print "Cache-control: no-cache"
+        print
+
+        # Generate web page
+        dataToolURL      =  "../../SubmitDatasetUI/html/SubmitDatasetDetails.html"                                 
+        mainURL          =  "../../../.."
+        resetURL         =  "../../SubmitDatasetUI/html/SubmitDatasetDetails.html?dir="+dirName
+        viewDatasetURL   =  "../../DisplayDataset/html/DisplayDataset.html?dir="+ dirName + "#"+ datasetUnzippedName 
+
+        pageTemplate = ("""
+            <html>
+                <head>
+                    <script type="text/javascript" src="../../jQuery/js/jquery-1.4.2.js"></script>
+                </head>
+                
+                <body>
+                    <h2>%(status)s</h2>
+                    <h3><a href="%(viewDatasetURL)s">View submitted dataset - %(datasetUnzippedName)s</a></h3>
+                    <h3><a href="%(resetURL)s" id="revised">Submit revised version</a></h3>
+                    <h3><a href="%(dataToolURL)s">Submit another dataset</a></h3>
+                    <h3><a href="%(mainURL)s">Return to ADMIRAL front page</a></h3>
+                </body>
+            </html>
+            """)
+        print (pageTemplate%
+            { 'viewDatasetURL'      : viewDatasetURL
+            , 'datasetName'         : datasetName
+            , 'dataToolURL'         : dataToolURL
+            , 'mainURL'             : mainURL
+            , 'datasetUnzippedName' : datasetUnzippedName
+            , 'status'              : status
+            , 'resetURL'            : resetURL               
+            })
+
+    return
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    form = cgi.FieldStorage()   # Parse the query
+    os.chdir("/home/")           # Base directory for admiral server data
+    datasetSummaryForm(form, sys.stdout)
+
+# End.
---
+++ src/SubmitDatasetHandler/tests/TestSubmitDataset.py
@@ -0,0 +1,217 @@
+# $Id: TestAll.py 1047 2009-01-15 14:48:58Z graham $
+#
+# Unit testing for WebBrick library functions (Functions.py)
+# See http://pyunit.sourceforge.net/pyunit.html
+#
+
+import sys, unittest, logging, zipfile, re
+from os.path import normpath
+
+#Add main library directory to python path
+###sys.path.append("../../../test")
+sys.path.append("..")
+
+from MiscLib import TestUtils
+import HttpUtils
+import SubmitDatasetUtils
+
+SiloName             =  "admiral-test"
+FileName             =  "file1.txt"
+Username             =  "admiral"
+Password             =  "admiral"
+TestDatasetName      =  "TestSubmitDataset"
+EmptyTestDatasetName =  "EmptyTestSubmitDataset"
+FileMimeType         =  "text/plain"
+ZipMimeType          =  "application/zip"
+DirName              =  "DatasetsTopDir"
+EmptyDirName         =  "DatasetsTopDir/DatasetsEmptySubDir"
+TestPat              =  re.compile("^.*$(?<!\.zip)")
+logger               =  logging.getLogger(TestDatasetName)
+
+class TestDatasetSubmission(unittest.TestCase):
+  
+    def setUp(self):
+        HttpUtils.setRequestUserPass(Username, Password)
+        return
+        
+    # Tests
+     
+        # Test the Dataset Creation: <TestSubmission>       
+    def testDatasetCreation(self):        
+        # Read original Dataset List from Silo
+        initialDatasetsFromSilo = SubmitDatasetUtils.getDatasetsListFromSilo(SiloName)
+        
+        # Formulate the initial datasets received form Databank Silo into a List: <initialDatasetsListFromSilo>
+        initialDatasetsListFromSilo = []
+        for initialDataset in initialDatasetsFromSilo:
+            initialDatasetsListFromSilo.append(initialDataset)
+            
+        # Create a new Test Dataset
+        SubmitDatasetUtils.createDataset(SiloName,TestDatasetName)
+        
+        # Read updated Dataset List from Silo
+        updatedDatasetsFromSilo = SubmitDatasetUtils.getDatasetsListFromSilo(SiloName)
+        
+        # Formulate the updated datasets received from Databank Silo into a List: <updatedDatasetListFromSilo>
+        updatedDatasetsListFromSilo = []
+        for updatedDataset in updatedDatasetsFromSilo:
+            updatedDatasetsListFromSilo.append(updatedDataset)
+
+        logger.debug("Updated no. of Datasets in Silo "+str(len(updatedDatasetsListFromSilo))+", Initial no. of Datasets in Silo  "+str(len(initialDatasetsListFromSilo)))
+       
+        # Test that the length <updatedDatasetListFromSilo> = length<initialDatasetsListFromSilo> + 1
+        self.assertEquals(len(updatedDatasetsListFromSilo), len(initialDatasetsListFromSilo)+1, "Dataset Created Successfully")
+        SubmitDatasetUtils.deleteDataset(SiloName,TestDatasetName+"-packed") 
+        return
+ 
+        # Test the Dataset Deletion :<TestSubmission>  
+    def testDatasetDeletion(self):  
+        SubmitDatasetUtils.createDataset(SiloName,TestDatasetName)
+        # Check dataset exists
+        response = HttpUtils.doHTTP_GET(
+            resource = "/" + SiloName + "/datasets/" + TestDatasetName+"-packed", 
+            expect_status=200, expect_reason="OK", accept_type="application/json")
+        # Test the dataset deletion      
+        SubmitDatasetUtils.deleteDataset(SiloName,TestDatasetName+"-packed") 
+        # Test dataset no longer exists
+        response = HttpUtils.doHTTP_GET(
+            resource = "/" + SiloName + "/datasets/" + TestDatasetName+"-packed", 
+            expect_status=404, expect_reason="Not Found")
+        return
+    
+        # Test the File Submission into the Dataset: <TestSubmission>  
+    def testSingleFileSubmission(self):     
+        SubmitDatasetUtils.createDataset(SiloName,TestDatasetName)  
+        # Submit a file to the Dataset
+        localMimeType = FileMimeType 
+        localFileContent  = SubmitDatasetUtils.getLocalFileContents(FileName)
+        SubmitDatasetUtils.submitFileToDataset(SiloName, TestDatasetName, FileName, localMimeType, FileName)
+        
+        # Read from the  updated Dataset
+        (remoteMimeType,remoteFileContent) = SubmitDatasetUtils.getFileFromDataset(SiloName, TestDatasetName+"-packed", FileName)
+        
+        # Check that the <localFileContent> = <remoteFileContents>
+        self.assertEqual(localMimeType, remoteMimeType, "Difference between local and remote MIME types")
+        self.assertEqual(localFileContent, remoteFileContent, "Difference between local and remote file contents")
+        SubmitDatasetUtils.deleteDataset(SiloName,TestDatasetName+"-packed")  
+        return
+    
+    def testDirectorySubmission(self):    
+        SubmitDatasetUtils.createDataset(SiloName,TestDatasetName)  
+        # Zip the required directory
+        zipFileName = SubmitDatasetUtils.zipLocalDirectory(DirName,TestPat,TestDatasetName+".zip")
+        #logger.debug("ZipFileName: " + zipFileName)
+        localZipFileContent  = SubmitDatasetUtils.getLocalFileContents(zipFileName)
+        SubmitDatasetUtils.submitFileToDataset(SiloName, TestDatasetName, zipFileName, ZipMimeType, zipFileName)
+
+        # Read from the  updated Dataset
+        (remoteMimeType,remoteZipFileContent) = SubmitDatasetUtils.getFileFromDataset(SiloName, TestDatasetName+"-packed", zipFileName)
+        
+        #logger.debug("LocalZipFileContents: " + localZipFileContent)
+        #logger.debug(" RemoteZipFileContents: " + remoteZipFileContent)
+        
+        # Check that the <localFileContent> = <remoteFileContents>
+        self.assertEqual(ZipMimeType, remoteMimeType, "Difference between local and remote zip MIME types") 
+        self.assertEqual(localZipFileContent, remoteZipFileContent, "Difference between local and remote zip files contents") 
+        
+        #unpack the contents
+        newDatasetname = SubmitDatasetUtils.unzipRemoteFileToNewDataset(SiloName, TestDatasetName, zipFileName)
+        SubmitDatasetUtils.deleteDataset(SiloName,TestDatasetName+"-packed")  
+        SubmitDatasetUtils.deleteDataset(SiloName,newDatasetname)
+        return
+    
+    def testSubsequentDatasetSubmission(self):    
+        SubmitDatasetUtils.createDataset(SiloName,TestDatasetName)  
+        # Zip the required directory
+        zipFileName = SubmitDatasetUtils.zipLocalDirectory(DirName,TestPat,TestDatasetName+".zip")
+        #logger.debug("ZipFileName: " + zipFileName)
+        localZipFileContent  = SubmitDatasetUtils.getLocalFileContents(zipFileName)
+        SubmitDatasetUtils.submitFileToDataset(SiloName, TestDatasetName, zipFileName, ZipMimeType, zipFileName)
+
+        #Resubmit the File to Dataset skipping dataset creation as the dataset already exists
+        SubmitDatasetUtils.submitFileToDataset(SiloName, TestDatasetName, zipFileName, ZipMimeType, zipFileName)
+        # Read from the  updated Dataset
+        (remoteMimeType,remoteZipFileContent) = SubmitDatasetUtils.getFileFromDataset(SiloName, TestDatasetName+"-packed", zipFileName)
+        
+        #logger.debug("LocalZipFileContents: " + localZipFileContent)
+        #logger.debug(" RemoteZipFileContents: " + remoteZipFileContent)
+        
+        # Check that the <localFileContent> = <remoteFileContents>
+        self.assertEqual(ZipMimeType, remoteMimeType, "Difference between local and remote zip MIME types") 
+        self.assertEqual(localZipFileContent, remoteZipFileContent, "Difference between local and remote zip files contents") 
+        
+        #unpack the contents
+        newDatasetname = SubmitDatasetUtils.unzipRemoteFileToNewDataset(SiloName, TestDatasetName, zipFileName)
+        SubmitDatasetUtils.deleteDataset(SiloName,TestDatasetName+"-packed")  
+        SubmitDatasetUtils.deleteDataset(SiloName,newDatasetname)
+        return
+
+    # To dotestDirectorySubmission
+    def testEmptyDirectorySubmission(self):
+            
+        # Create a new Test Dataset
+        SubmitDatasetUtils.createDataset(SiloName,EmptyTestDatasetName)
+
+        # Zip the Empty directory
+        zipFileName = SubmitDatasetUtils.zipLocalDirectory(EmptyDirName,TestPat,EmptyTestDatasetName+".zip")
+        #logger.debug("ZipFileName: " + zipFileName)
+        localZipFileContent  = SubmitDatasetUtils.getLocalFileContents(zipFileName)
+        SubmitDatasetUtils.submitFileToDataset(SiloName,EmptyTestDatasetName, zipFileName, ZipMimeType, zipFileName)
+
+        # Read from the  updated Dataset
+        (remoteMimeType, remoteZipFileContent) = SubmitDatasetUtils.getFileFromDataset(SiloName, EmptyTestDatasetName+"-packed", zipFileName)
+        
+        #logger.debug("LocalZipFileContents: " + localZipFileContent)
+        #logger.debug(" RemoteZipFileContents: " + remoteZipFileContent)
+        
+        # Check that the <localFileContent> = <remoteFileContents>
+        self.assertEqual(ZipMimeType, remoteMimeType, "Difference between local and remote zip MIME types") 
+        self.assertEqual(localZipFileContent, remoteZipFileContent, "Difference between local and remote zip file contents") 
+        
+        #unpack the contents
+        newDatasetName = SubmitDatasetUtils.unzipRemoteFileToNewDataset(SiloName, EmptyTestDatasetName, zipFileName)
+        SubmitDatasetUtils.deleteDataset(SiloName,EmptyTestDatasetName+"-packed")
+        SubmitDatasetUtils.deleteDataset(SiloName,newDatasetName)
+        return
+
+    def tearDown(self):
+        
+        return
+    
+def getTestSuite(select="unit"):
+    """
+    Get test suite
+
+    select  is one of the following:
+            "unit"      return suite of unit tests only
+            "component" return suite of unit and component tests
+            "all"       return suite of unit, component and integration tests
+            "pending"   return suite of pending tests
+            name        a single named test to be run
+    """
+    testdict = {
+        "unit":
+            [ #"testUnits"
+              "testDatasetCreation"
+            , "testSingleFileSubmission"
+            , "testDirectorySubmission"
+            , "testSubsequentDatasetSubmission"
+            , "testEmptyDirectorySubmission"
+            , "testDatasetDeletion"
+            ],
+        "component":
+            [ #"testComponents"
+            ],
+        "integration":
+            [ #"testIntegration"
+            ],
+        "pending":
+            [ #"testPending"
+            ]
+        }
+    return TestUtils.getTestSuite(TestDatasetSubmission, testdict, select=select)
+
+if __name__ == "__main__":
+    TestUtils.runTests("TestSubmitDataset.log", getTestSuite, sys.argv)
+    #runner = unittest.TextTestRunner()
+    #runner.run(getTestSuite())
