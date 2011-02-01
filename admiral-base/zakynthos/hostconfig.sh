120b7501c28a2cfae07bdba3faaf1306a0b46906
graham <graham@localhost>
2011-02-01 11:44:22 +0000

    Delete some old spike files

---
+++ admiral-base/zakynthos/hostconfig.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Configuration parameters for host Zakynthos
+#
+# (See also: admiralconfig.sh)
+#
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="TestGroup"
+
+# Username initially designated as research group leader
+# (System scripts will attempt to ensure that there is always a user 
+# who is the designated research group leader)
+LEADERNAME="TestLeader"
+
+# Databank host and silo names
+
+#DATABANKHOST="163.1.127.173"
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral-test"
+
+# Override IP address for testing (kos.zoo.x.ac.uk)
+# IPADDR="129.67.26.204"
+
+# End.
+
---
+++ admiral-base/zoo-admiral-ibrg/hostconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-devel
+#
+# (See also: admiralconfig.sh)
+#
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="SHOTTON"
+
+# Username initially designated as research group leader
+# (System scripts will attempt to ensure that there is always a user 
+# who is the designated research group leader)
+LEADERNAME="DavidShotton"
+
+# Databank host and silo names
+
+#DATABANKHOST="163.1.127.173"
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral-test"
+
+# End.
+
