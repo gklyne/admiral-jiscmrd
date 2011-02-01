6625de4fef5f74d97dccb46c729cf3012852b04d
bhavana <bhavana@localhost>
2011-02-01 12:03:17 +0000

    Changed logo path to appear on the ADMIRAL main page.

---
+++ admiral-base/admiraluserdel.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source /root/admiralconfig.d/admiralconfig.sh
+
+if [[ "$1" == "" ]]; then
+  echo "Usage: $0 username [purge]"
+  exit 1
+elif [[ "$1" == "$RGLeaderName" ]]; then
+  echo "Do not use this script to delete the Research Group Leader!"
+  echo "Work out who is to be the new Research Group Leader user."
+  echo "Then run the 'admiralleaderchange.sh' script."
+  exit 1
+else 
+
+  if [[ "$2" == "purge" ]]; then
+
+    rm -rf "/home/data/private/$1"
+    rm -rf "/home/data/shared/$1"
+    rm -rf "/home/data/collab/$1"
+    rm -rf "/mnt/lv-admiral-data/home/$1"
+    rm -rf "/home/$1"
+    rm -rf "/home/$1-saved"
+    rm -rf "/home/$1-deleted"
+    
+    rm -f /etc/apache2/conf.d/user.$1
+    rm -f /root/admiralconfig.d/admiralresearchgroupmembers/$1.sh
+    rm -f /etc/apache2/conf.d/orphan.$1
+    rm -f /root/admiralconfig.d/admiralresearchgrouporphans/$1.sh
+    
+  else
+
+    chown -R admiral-orphan:RGOrphan /home/data/private/$1
+    chown -R admiral-orphan:RGOrphan /home/data/shared/$1
+    chown -R admiral-orphan:RGOrphan /home/data/collab/$1
+    chown -R admiral-orphan:RGOrphan /mnt/lv-admiral-data/home/$1
+    # (-L tests for symlink ...)
+    if [[ -L "/home/$1" ]]; then
+      rm "/home/$1"
+    elif [[ -e "/home/$1" ]]; then
+      mv "/home/$1" "/home/$1-deleted"
+    fi
+    if [[ -e "/home/$1-saved" ]]; then
+      mv /home/$1-saved /home/$1
+    fi
+  
+    mv /etc/apache2/conf.d/user.$1 /etc/apache2/conf.d/orphan.$1
+    mkdir -p /root/admiralconfig.d/admiralresearchgrouporphans
+    mv /root/admiralconfig.d/admiralresearchgroupmembers/$1.sh /root/admiralconfig.d/admiralresearchgrouporphans/
+  fi
+   
+  smbldap-userdel -r $1
+ 
+fi
+
+# End.
---
+++ admiral-base/createapacheuserconfig.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+    echo "Usage:"
+    echo "  $0 username [orphan]" 
+    echo "      Generate Apache access configuration for named user (or deleted user)" 
+    echo ""
+    echo "  $0 all"
+    echo "      Generate Apache access configuration for all configured ADMIRAL users" 
+    echo ""
+    exit
+fi
+
+if [[ "$1" != "all" ]]; then
+    if [[ ! -e /root/admiralconfig.d/admiralresearchgroupmembers/$1.sh ]]; then
+        echo "No such user: $1"
+        exit
+    fi
+fi
+ 
+source /root/admiralconfig.d/admiralconfig.sh
+
+function generateuserconfigfile()
+{
+    # $1 = users script name
+    # $2 = "user" / "orphan"
+ 
+    source $1
+    echo $username $userfullname $userrole
+    
+    # Set up Apache access control configuration
+
+  cat << EOF > /etc/apache2/conf.d/$2.$username
+<Location /data/private/$username>
+    Order Deny,Allow
+    Allow from all
+    <LimitExcept REPORT GET OPTIONS PROPFIND>
+      Require user $username
+    </LimitExcept>
+    <Limit PROPFIND OPTIONS GET REPORT>
+      # NOTE:
+      # Tried to use a combination of "Require user" and "Require ldap-attribute"
+      # here, but this caused access failures for all users.
+      # TestLeader is included here for testing only.
+    Require user $username $RGLeaderName
+    </Limit>
+</Location>
+
+<Location /data/shared/$username>
+    Order Deny,Allow
+    Allow from all
+    <LimitExcept REPORT GET OPTIONS PROPFIND>
+      Require user $username
+    </LimitExcept>
+    <Limit PROPFIND OPTIONS GET REPORT>
+      Require ldap-attribute gidNumber=$RGLeaderGID
+      Require ldap-attribute gidNumber=$RGMemberGID
+    </Limit>
+</Location>
+
+<Location /data/collab/$username>
+    Order Deny,Allow
+    Allow from all
+    <LimitExcept REPORT GET OPTIONS PROPFIND>
+      Require user $username
+    </LimitExcept>
+    <Limit PROPFIND OPTIONS GET REPORT>
+      Require ldap-attribute gidNumber=$RGLeaderGID
+      Require ldap-attribute gidNumber=$RGMemberGID
+      Require ldap-attribute gidNumber=$RGCollabGID
+    </Limit>
+</Location>
+
+EOF
+  chown root:root /etc/apache2/conf.d/$2.$username
+  chmod 644 /etc/apache2/conf.d/$2.$username
+}
+
+# Process all user files in /root/admiralconfig.d/a/root/admiralresearchgroupmembers
+
+if [[ "$1" == "all" ]]; then
+    for u in `ls /root/admiralconfig.d/admiralresearchgroupmembers/*.sh`; do
+        generateuserconfigfile $u user
+    done
+    for u in `ls /root/admiralconfig.d/admiralresearchgrouporphans/*.sh`; do
+        generateuserconfigfile $u orphan
+    done
+else
+    usertype=user
+    userdir=admiralresearchgroupmembers
+    if [[ "$2" == "orphan" ]]; then
+        usertype=orphan
+        userdir=admiralresearchgrouporphans
+    fi
+    generateuserconfigfile /root/admiralconfig.d/$userdir/$1.sh $usertype  
+fi
+
+#echo ==================================================================
+#echo "Remember to restart Apache server to use the revised configuration"
+#echo ==================================================================
+#echo "# /etc/init.d/apache2 restart"
+#echo
+
+echo ======================================================
+echo "Restart Apache server to use the revised configuration"
+echo ======================================================
+echo
+
+/etc/init.d/apache2 restart"
+
+# End.
+  
