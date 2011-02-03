c96f5e17d10d2ea9bac0e07cabaf757f28f830db
bhavana <bhavana@localhost>
2011-02-03 17:00:02 +0000

    spike testing code - ajax

---
+++ admiral-base/admiraltoolsetup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# source /root/admiralconfig.d/admiralconfig.sh
+
+echo ==========================================
+echo "Extract admiral tools from code repository"
+echo ==========================================
+echo TODO: tag stable version of tools and use that
+echo NOTE: tool directories are owned by root, with RO access for all
+echo NOTE: see also /etc/apache2/sites-enabled/default-ssl
+
+if [[ ! -e /mnt/lv-admiral-data/tool/admiral ]]; then
+    mkdir -p /mnt/lv-admiral-data/tool
+    cd /mnt/lv-admiral-data/tool
+    hg clone https://admiral-jiscmrd.googlecode.com/hg/ admiral
+    cd /root
+fi
+
+# End.
