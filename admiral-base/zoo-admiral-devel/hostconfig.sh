bee94ce16c5b278a85439c804259ab2f4e20a8e7
graham <graham@localhost>
2011-03-24 10:42:10 +0000

    Display message when submitting dataset to Databank repo

---
+++ admiral-base/zoo-admiral-devel/hostconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-devel
+#
+# (See also: admiralconfig.sh)
+#
+
+RESEARCHGROUPNAME="Evolutionary Development"
+
+ADMIRALDOMAINDC="zoo ox ac uk"
+ADMIRALHOSTNAME="zoo-admiral-devel"
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="EvolutionaryDevelopment"
+
+# Databank host and silo names
+
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral-test"
+
+# End.
+
